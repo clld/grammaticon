7e0406c7a5be63c9ac0d587275fe7cc38baff570
chrzyki <christoph@functor-argument.net>
2018-02-03 13:45:31 +0100

    Updated scaffolding etc.

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
 from setuptools import setup, find_packages
 
-
-requires = [
-    'clldmpg~=3.1',
-    'markdown',
-    'waitress'
-]
-
-
 setup(
     name='grammaticon',
     version='0.0',
     description='grammaticon',
     long_description='',
-    classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    install_requires = [
+        'clld~=4.0',
+        'clldmpg~=3.1.1',
+        'markdown',
+        'sqlalchemy',
+        'waitress',
     ],
     extras_require={
-        'dev': ['flake8', 'waitress', 'psycopg2'],
-        'test': [
+        'dev': [
+            'flake8',
             'tox',
+            'psycopg2',
+        ],
+        'test': [
             'mock',
             'pytest>=3.1',
             'pytest-clld',
@@ -33,6 +29,12 @@ setup(
             'zope.component>=3.11.0',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+    ],
     author='',
     author_email='',
     url='',
@@ -40,7 +42,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=requires,
     test_suite="grammaticon",
     entry_points="""\
 [paste.app_factory]
